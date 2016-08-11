d950dfa80412b6a271f51ab76cd65aee08fb9a45
Taylor Robinson <taylor@appbackr.com>
2016-08-11 11:41:10 -0700

    don't import greetings model

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
-from .models import Greeting
 import json, random, requests
 import os
 import psycopg2
