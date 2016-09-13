dda4467a7d344f3dd4cc0ce758d31f75fe7099f2
Taylor Robinson <taylor@appbackr.com>
2016-09-12 22:12:44 -0700

    existing redirect uri

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'^auth-cal-success', hello.views.authCalendarSuccess, name='authCalendarSuccess'),
     url(r'^auth', hello.views.auth, name='auth'),
     url(r'^catchtoken2', hello.views.catchToken2, name='catchToken2'),
+    url(r'^auth-calendar-success', hello.views.catchToken2, name='catchToken2'),
     url(r'^catchtoken', hello.views.catchToken, name='catchToken'),
     url(r'^receive-gcal', hello.views.receiveGcal, name='receiveGcal')
     #url(r'^admin/', include(admin.site.urls)),
