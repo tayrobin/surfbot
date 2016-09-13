e662b79abc4ad9a14aa551b401ddb3ce796dbf85
Taylor Robinson <taylor@appbackr.com>
2016-09-12 21:53:17 -0700

    import redirect

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 import json, random, requests, os, psycopg2, urlparse, uuid
 from django.views.decorators.csrf import csrf_exempt
